3246707ecaa688f184cca933b10b3ea2bb6df6f7
Juergen Edelbluth <juergen@edelbluth.eu>
2014-09-19 17:17:53 +0200

    Added setup.py

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='arobito',
+    version='',
+    packages=['arobito', 'arobito.controlinterface'],
+    url='',
+    license='Apache License v2.0',
+    author='The Arobito Project',
+    author_email='',
+    description=''
+)
